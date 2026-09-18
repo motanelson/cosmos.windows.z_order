549566859655ec86fd34acdca98bfb4bfc7f4c82
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-09-18 19:05:48 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Image i; Bitmap b; int value = 0; int value2 = 0; int xx = 0; int yy = 0; Graphics ee; int xxx = 0; int yyy = 0; int xxxx = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void moveTop(int x, int y) 
+        {
+            drawWin(x, y);
+        
+        }
+        private void drawWin(int x,int y) 
+        {
+            Pen ppp = new Pen(Color.FromArgb(0, 0, 0));
+            Pen ppp2 = new Pen(Color.FromArgb(255, 255, 255));
+            Brush p1 = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
+            Brush p2 = new SolidBrush(Color.FromArgb(255, 0, 0, 0));
+            Rectangle r = new Rectangle(x, y, 100, 100);
+            Rectangle rr = new Rectangle(x, y, 100, 10);
+            ee = Graphics.FromImage(b);
+            ee.FillRectangle(p1, r);
+            ee.DrawRectangle(ppp, r);
+            ee.FillRectangle(p2, rr);
+            ee.DrawRectangle(ppp2, rr);
+
+
+            ppp.Dispose();
+            p1.Dispose();
+            p2.Dispose();
+            ppp2.Dispose();
+            ee.Dispose();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double[] dcos = { 1.00, 0.707, 0.00, -0.707, -1.00, -0.707, 0.00, 0.707, 1.00 };
+            double[] dsin = { 0.00, 0.707, 1.00, 0.707, 0.00, -0.707, -1.00, -0.707, 0.0 };
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = b;
+            xxx = pictureBox1.Width / 2;
+            yyy = pictureBox1.Height / 2;
+            
+            
+            for (int a = 7; a > -1; a--) {
+                drawWin(a * 10+10, a * 10+10);
+               
+
+            }
+            int www = 4;
+            //moveTop(www * 10, www * 10);
+           
+
+        }
+    }
+}
